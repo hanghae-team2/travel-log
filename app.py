b3dd0885bfad5bc02f794f4d219f91927bb70855
Joonyoung Hong <hjunyoung10@gmail.com>
2023-10-11 15:32:05 +0900

    Add signin page

--- app.py
+++ app.py
@@ -27,5 +27,10 @@ def login():
     return render_template('login.html')
 
 
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
