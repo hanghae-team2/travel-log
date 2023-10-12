7e00e0122a8f7600c212ec52cc6667b27bf805da
Joonyoung Hong <hjunyoung10@gmail.com>
2023-10-12 09:24:38 +0900

    Add jwt token genrate feature

--- app.py
+++ app.py
@@ -3,11 +3,15 @@
 0. Flask : 웹서버를 시작할 수 있는 기능. app이라는 이름으로 플라스크를 시작한다
 1. render_template : html파일을 가져와서 보여준다
 '''
+from datetime import datetime, timezone, timedelta
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Session
 from flask_bcrypt import Bcrypt
 import os
-from flask import Flask, render_template, request, redirect, url_for
+import jwt
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
@@ -70,19 +74,21 @@ def registration():
     return render_template('registration.html')
 
 
+# JWT token generator
+def generate_jwt_token(username):
+    jwt_token = jwt.encode({
+        "username": username,
+        "exp": datetime.now(tz=timezone.utc) + timedelta(seconds=int(os.environ.get('JWT_EXPIRES_SEC')))},  # TODO(Joonyoung) exp 시간 변경
+        os.environ.get('JWT_SECRET'), algorithm="HS256")
+
+    # decoded = jwt.decode(jwt_token, os.environ.get(
+    #     "JWT_SECRET"), algorithms=["HS256"])
+    return jwt_token
+
+
 @app.route('/login', methods=['GET'])
 def getLogin():
     return render_template('login.html')
-
-
-# users = [{
-#     "username": 'bob1234',
-#     "password": "$2b$12$ZRqQ2WM/UWcFlHyTSmWb5upnpB3YzDJw97/.lgkce1e6pqcodVym6"
-# },
-#     {
-#     "username": 'jun1234',
-#     "password": "$2b$12$ZRqQ2WM/UWcFlHyTSmWb5upnpB3YzDJw97/.lgkce1e6pqcodVym6"
-# }]
 
 
 @app.route('/login', methods=['POST'])
@@ -99,15 +105,21 @@ def postLogin():
 
     # user가 있는 경우
     password = request.form.get('password')
-
     password_match = bcrypt.check_password_hash(user[0].password, password)
 
-    # # password 틀린 경우
+    # password 틀린 경우
     if not password_match:
         print('wrong password')
         return render_template('login.html', error_message=error_message)
 
-    return redirect(url_for('home'))
+    # password 일치하는 경우 JWT token 생성
+    token = generate_jwt_token(username)
+
+    # res = make_response(redirect('home'))
+    res = make_response(redirect('test_jwt'))
+    res.set_cookie('token', token, max_age=int(
+        int(os.environ.get('JWT_EXPIRES_SEC'))), httponly=True, secure=True, samesite="none")
+    return res
 
 
 @app.route('/signup',  methods=['GET'])
@@ -162,6 +174,11 @@ def post(id):
         "user_id": post.user_id
     }
     return render_template('post.html', data=data)
+
+
+@app.route('/test_jwt')
+def test_jwt():
+    return render_template('test_jwt.html')
 
 
 if __name__ == "__main__":
